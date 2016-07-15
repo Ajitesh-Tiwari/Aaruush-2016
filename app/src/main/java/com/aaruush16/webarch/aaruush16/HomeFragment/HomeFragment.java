4e1375e7d1c88abce9f878f053b583d8a6df2f0f
Ravi <ravitripathi1996@gmail.com>
2016-07-15 21:48:08 +0530

    Minor change in layout

--- app/src/main/java/com/aaruush16/webarch/aaruush16/HomeFragment/HomeFragment.java
+++ app/src/main/java/com/aaruush16/webarch/aaruush16/HomeFragment/HomeFragment.java
@@ -20,6 +20,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
