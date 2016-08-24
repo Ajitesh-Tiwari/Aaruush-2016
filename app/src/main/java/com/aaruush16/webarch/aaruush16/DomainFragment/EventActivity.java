70c6f7c68ffa8e4c18f33bb614bc4ee21177fd44
Ravi <ravitripathi1996@gmail.com>
2016-08-25 04:15:30 +0530

    Redisgned EventActivity. Added Favourite FAB

--- app/src/main/java/com/aaruush16/webarch/aaruush16/DomainFragment/EventActivity.java
+++ app/src/main/java/com/aaruush16/webarch/aaruush16/DomainFragment/EventActivity.java
@@ -1,11 +1,16 @@
 package com.aaruush16.webarch.aaruush16.DomainFragment;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +31,7 @@ public class EventActivity extends AppCompatActivity {
     Realm realm;
     Event event;
     ImageView event_img;
+    CollapsingToolbarLayout mCollapse;
     TextView txt_description,txt_date,txt_contact,txt_cost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +44,18 @@ public class EventActivity extends AppCompatActivity {
         event=new Event();
         event=query.findFirst();
 
-        ActionBar actionBar=getSupportActionBar();
-        actionBar.setTitle(event.getName());
+        mCollapse = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        mCollapse.setCollapsedTitleTextAppearance(R.style.CollapsedAppBar);
+        mCollapse.setExpandedTitleTextAppearance(R.style.ExpandedAppBar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.BLUE);
+        toolbar.setTitle(event.getName());
+
+        setSupportActionBar(toolbar);
+
+//        ActionBar actionBar=getSupportActionBar();
+//        actionBar.setTitle(event.getName());
 
 
         event_img= (ImageView) findViewById(R.id.event_img);
@@ -54,5 +70,25 @@ public class EventActivity extends AppCompatActivity {
         txt_date.setText(event.getDate());
         txt_contact.setText(event.getContact());
         txt_cost.setText(event.getCost());
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            int i=0;
+            @Override
+            public void onClick(View view) {
+                if(i==0) {
+                    fab.setImageResource(R.drawable.ic_star_white_24dp);
+                    Snackbar.make(view, "Added To Favourites", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    i++;
+                }
+                else
+                {
+                    fab.setImageResource(R.drawable.ic_star_border_white_24dp);
+                    Snackbar.make(view, "Removed from Favourites", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    i=0;
+                }
+            }
+        });
     }
 }
